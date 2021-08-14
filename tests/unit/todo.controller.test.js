8052350044aec8ce0f9c6ab68a0775ba3796c10f
Ankit <ankit.sanghavi87@gmail.com>
2021-08-14 18:32:33 +0530

    kuch toh

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,20 +1,37 @@
 const TodoController = require('../../controllers/test.controller')
 const TodoModel = require('../../model/todo.model')
 const httpMocks = require('node-mocks-http')
+const newTodo = require('../mock-data/new-todo.json')
 
 TodoModel.create = jest.fn();
 
+let req, res, next;
+beforeEach(() => {
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    next = null
+})
+
 describe("basic test to describe create todo", () => {
+    beforeEach(() => {
+        req.body = newTodo 
+    })
+
     it('does createTodo exist', () => {
         expect(typeof TodoController.createTodo).toBe("function");
     });
     it('does it call the create method', () => {
-        let req, res, next;
-        req = httpMocks.createRequest();
-        req = httpMocks.createResponse();
-        next = null
         TodoController.createTodo(req,res,next)
         expect(TodoModel.create).toBeCalled();
     })
-
+    it('response status code check and send check', () => {
+        TodoController.createTodo(req, res, next)
+        expect(res.statusCode).toBe(201)
+        expect(res._isEndCalled()).toBeTruthy()
+    })
+    it('tested mock response', () => {
+        TodoModel.create.mockReturnValue(newTodo)
+        TodoController.createTodo(req, res, next)
+        expect(res._getJSONData()).toStrictEqual(newTodo)
+    })
 })
