1dac21df5d60fecb6c6ca2da399857a28f3f4721
Ankit <ankit.sanghavi87@gmail.com>
2021-08-14 18:03:00 +0530

    model test

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,3 +1,20 @@
 const TodoController = require('../../controllers/test.controller')
+const TodoModel = require('../../model/todo.model')
+const httpMocks = require('node-mocks-http')
 
-d
+TodoModel.create = jest.fn();
+
+describe("basic test to describe create todo", () => {
+    it('does createTodo exist', () => {
+        expect(typeof TodoController.createTodo).toBe("function");
+    });
+    it('does it call the create method', () => {
+        let req, res, next;
+        req = httpMocks.createRequest();
+        req = httpMocks.createResponse();
+        next = null
+        TodoController.createTodo(req,res,next)
+        expect(TodoModel.create).toBeCalled();
+    })
+
+})
